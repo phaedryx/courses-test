b15b59016a47cb94c484a839be8d0ae0fe8c927e
Tad Thorley <phaedryx@gmail.com>
2013-03-25 16:34:01 -0600

    parser can identify files correctly

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,7 +1,26 @@
+require 'csv'
+
 class Parser
+  COURSES_HEADERS     = ["course_id", "course_name", "state"]
+  ENROLLMENTS_HEADERS = ["course_id", "user_id", "state"]
+  USERS_HEADERS       = ["user_id", "user_name", "state"]
+
   def initialize(args)
     
   end
   
-  
+  def self.courses_file?(path)
+    file_headers = CSV.open(path).shift
+    file_headers == COURSES_HEADERS
+  end
+
+  def self.enrollments_file?(path)
+    file_headers = CSV.open(path).shift
+    file_headers == ENROLLMENTS_HEADERS    
+  end
+
+  def self.users_file?(path)
+    file_headers = CSV.open(path).shift
+    file_headers == USERS_HEADERS
+  end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -3,5 +3,33 @@ require 'minitest/autorun'
 require 'parser'
 
 describe Parser do
-  
+  describe ".courses_file?" do
+    it "must return true if the csv is a list of courses" do
+      Parser.courses_file?("data/001.csv").must_equal true
+    end
+
+    it "must return false if the csv file is not a list of courses" do
+      Parser.courses_file?("data/002.csv").must_equal false
+    end
+  end
+
+  describe ".enrollments_file?" do
+    it "must return true if the csv file is a list of enrollments" do
+      Parser.enrollments_file?("data/003.csv").must_equal true
+    end
+
+    it "must return false if the csv file is not a list of enrollments" do
+      Parser.enrollments_file?("data/001.csv").must_equal false
+    end
+  end
+
+  describe ".users_file?" do
+    it "must return true if the csv file is a list of users" do
+      Parser.users_file?("data/002.csv").must_equal true
+    end
+
+    it "must return false if the csv file is not a list of users" do
+      Parser.users_file?("data/001.csv").must_equal false
+    end
+  end
 end
