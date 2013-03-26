7412fb003fdc5cbb694385f75b8dd6f3071af8cf
Tad Thorley <phaedryx@gmail.com>
2013-03-25 19:02:33 -0600

    less objects, cleaner tests

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -4,38 +4,41 @@ require 'parser'
 
 describe Parser do
   describe "#courses_file?" do
+    before do
+      @parser = Parser.new
+    end
     it "must return true if the csv is a list of courses" do
-      parser = Parser.new
-      parser.courses_file?("data/001.csv").must_equal true
+      @parser.courses_file?("data/001.csv").must_equal true
     end
 
     it "must return false if the csv file is not a list of courses" do
-      parser = Parser.new
-      parser.courses_file?("data/002.csv").must_equal false
+      @parser.courses_file?("data/002.csv").must_equal false
     end
   end
 
   describe "#enrollments_file?" do
+    before do
+      @parser = Parser.new
+    end
     it "must return true if the csv file is a list of enrollments" do
-      parser = Parser.new
-      parser.enrollments_file?("data/003.csv").must_equal true
+      @parser.enrollments_file?("data/003.csv").must_equal true
     end
 
     it "must return false if the csv file is not a list of enrollments" do
-      parser = Parser.new
-      parser.enrollments_file?("data/001.csv").must_equal false
+      @parser.enrollments_file?("data/001.csv").must_equal false
     end
   end
 
   describe "#users_file?" do
+    before do
+      @parser = Parser.new
+    end
     it "must return true if the csv file is a list of users" do
-      parser = Parser.new
-      parser.users_file?("data/002.csv").must_equal true
+      @parser.users_file?("data/002.csv").must_equal true
     end
 
     it "must return false if the csv file is not a list of users" do
-      parser = Parser.new
-      parser.users_file?("data/001.csv").must_equal false
+      @parser.users_file?("data/001.csv").must_equal false
     end
   end
 end
