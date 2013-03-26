026bc4edfc063f351134de74b96f2bf0201724ac
Tad Thorley <phaedryx@gmail.com>
2013-03-25 19:17:17 -0600

    basic file parsing

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,26 +1,84 @@
 require 'csv'
+require_relative 'course'
+require_relative 'enrollment'
+require_relative 'user'
 
 class Parser
+  class UnknownFileError < StandardError;  end
+
   COURSES_HEADERS     = ["course_id", "course_name", "state"]
   ENROLLMENTS_HEADERS = ["course_id", "user_id", "state"]
   USERS_HEADERS       = ["user_id", "user_name", "state"]
 
-  def initialize(args)
-    
+  attr_reader :courses, :enrollments, :users
+
+  def initialize()
+    @courses     = []
+    @users       = []
+    @enrollments = []
   end
   
-  def self.courses_file?(path)
+  def import_file!(path)
+    if courses_file?(path)
+      import_courses(path)
+    elsif enrollments_file?(path)
+      import_enrollments(path)
+    elsif users_file?(path)
+      import_users(path)
+    else
+      raise UnknownFileError, "File doesn't match a known file format"
+    end
+  end
+
+  def courses_file?(path)
     file_headers = CSV.open(path).shift
     file_headers == COURSES_HEADERS
   end
 
-  def self.enrollments_file?(path)
+  def enrollments_file?(path)
     file_headers = CSV.open(path).shift
     file_headers == ENROLLMENTS_HEADERS    
   end
 
-  def self.users_file?(path)
+  def users_file?(path)
     file_headers = CSV.open(path).shift
     file_headers == USERS_HEADERS
+  end
+
+private
+  def import_courses(path)
+    CSV.foreach(path, headers: true) do |row|
+      course = Course.new(course_id: row["course_id"], course_name: row["course_name"], state: row["state"])
+      index = @courses.index(course)
+      if index
+        @courses[index] = course
+      else
+        @courses << course
+      end
+    end
+  end
+
+  def import_enrollments(path)
+    CSV.foreach(path, headers: true) do |row|
+      enrollment = Enrollment.new(course_id: row["course_id"], user_id: row["user_id"], state: row["state"])
+      index = @enrollments.index(enrollment)
+      if index
+        @enrollments[index] = enrollment
+      else
+        @enrollments << enrollment
+      end
+    end
+  end
+
+  def import_users(path)
+    CSV.foreach(path, headers: true) do |row|
+      user = User.new(user_id: row["user_id"], user_name: row["user_name"], state: row["state"])
+      index = @users.index(user)
+      if index
+        @users[index] = user
+      else
+        @users << user
+      end
+    end
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -7,6 +7,7 @@ describe Parser do
     before do
       @parser = Parser.new
     end
+
     it "must return true if the csv is a list of courses" do
       @parser.courses_file?("data/001.csv").must_equal true
     end
@@ -20,6 +21,7 @@ describe Parser do
     before do
       @parser = Parser.new
     end
+
     it "must return true if the csv file is a list of enrollments" do
       @parser.enrollments_file?("data/003.csv").must_equal true
     end
@@ -33,12 +35,41 @@ describe Parser do
     before do
       @parser = Parser.new
     end
+
     it "must return true if the csv file is a list of users" do
       @parser.users_file?("data/002.csv").must_equal true
     end
 
     it "must return false if the csv file is not a list of users" do
       @parser.users_file?("data/001.csv").must_equal false
+    end
+  end
+
+  describe "#import_file!" do
+    before do
+      @parser = Parser.new
+    end
+
+    it "must raise an error if it isn't a known type" do
+      -> { @parser.import_file!("test_data/empty.csv") }.must_raise Parser::UnknownFileError
+    end
+
+    it "must import course files" do
+      course = Course.new(course_id: "C628944", course_name: "Suicide", state: "active")
+      @parser.import_file!("test_data/course.csv")
+      @parser.courses.include?(course).must_equal true
+    end
+
+    it "must import enrollment files" do
+      enrollment = Enrollment.new(course_id: "C126767", user_id: "U511908", state: "active")
+      @parser.import_file!("test_data/enrollment.csv")
+      @parser.enrollments.include?(enrollment).must_equal true
+    end
+
+    it "must import user files" do
+      user = User.new(user_id: "U531649", username: "Noah Thomas", state: "active")
+      @parser.import_file!("test_data/user.csv")
+      @parser.users.include?(user).must_equal true
     end
   end
 end
